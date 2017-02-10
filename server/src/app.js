b7f0a831d6b888e6b266ec26fb0adcd35ec36986
Kevin Houck <kevin.houck.95@gmail.com>
2017-02-10 01:18:22 -0600

    Fix server start again

--- server/src/app.js
+++ server/src/app.js
@@ -10,22 +10,22 @@ import config from './config'
 import Logger from './logging/logger'
 
 let app = express();
-
+let server = null
 if (config.node_env === 'production' && config.https === true) {
   const options = {
     key: fs.readFileSync('/etc/letsencrypt/live/padshare.io/privkey.pem'),
     cert: fs.readFileSync('/etc/letsencrypt/live/padshare.io/cert.pem')
   }
-  var server = https.createServer(options, app)
+  server = https.createServer(options, app)
 } else {
-  var server = http.createServer(app)
+  server = http.createServer(app)
 }
 
 const logger = new Logger(config.logging.verbosity)
 let io = new SocketIO(server);
 
-var namespaces = {}
-var database = new DatabaseProxy(config.redis.hostname, config.redis.port, logger)
+let namespaces = {}
+let database = new DatabaseProxy(config.redis.hostname, config.redis.port, logger)
 
 app.use(morgan('combined'))
 app.use(cors());
