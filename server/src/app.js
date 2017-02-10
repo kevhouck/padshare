468019e8f7552337dcc58b13d626b9b93cec9189
Kevin Houck <kevin.houck.95@gmail.com>
2017-02-10 00:19:39 -0600

    Modify server ports

--- server/src/app.js
+++ server/src/app.js
@@ -20,11 +20,12 @@ app.use(cors());
 
 setupRoutes(app, io, database, namespaces, logger)
 
-http.createServer(app).listen(80)
 if (config.node_env === 'production' && config.https === true) {
   const options = {
     key: fs.readFileSync('/etc/letsencrypt/live/padshare.io/privkey.pem'),
     cert: fs.readFileSync('/etc/letsencrypt/live/padshare.io/cert.pem')
   }
-  https.createServer(options, app).listen(443)
+  https.createServer(options, app).listen(3000)
+} else {
+  http.createServer(app).listen(3000)
 }
