1a1c3d6e9f5b174621d8dd848e18285b23010774
Kevin <kevin.houck.95@gmail.com>
2017-02-11 18:02:06 -0600

    Get webpack dev server working

--- webapp/webpack.config.dev.js
+++ webapp/webpack.config.dev.js
@@ -1,16 +1,14 @@
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var webpack = require('webpack');
+var path = require("path");
 
 module.exports = {
-    entry: [
-        './src/index.js',
-        'webpack/hot/only-dev-server',
-        'webpack-dev-server/client?http://localhost:8081'
-    ],
+    entry: {
+      app: ['./src/index.js', './src/style/index.css', './src/index.html']
+    },
     output: {
-        path: __dirname + '/dist',
-        filename: "index_bundle.js",
-        publicPath: "http://localhost:8080/assets/",
+        path: path.resolve(__dirname, '/dist'),
+        filename: "index.bundle.js",
     },
     module: {
         loaders: [
@@ -18,17 +16,16 @@ module.exports = {
             { test: /\.js$/, exclude: [/node_modules/], loaders: ['babel-loader']},
             { test: /\.css$/, loader: "style-loader!css-loader" },
             { test: /\.png$/, loader: "url-loader?limit=100000" },
-            { test: /\.jpg$/, loader: "file-loader" },
+            { test: /\.jpg$/, loader: "file-loader" }
         ]
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: __dirname + '/src/index.html',
+            template: path.resolve(__dirname, 'src/index.html'),
             filename: 'index.html',
             inject: 'body'
         }),
-        new webpack.HotModuleReplacementPlugin(),
-      new webpack.EnvironmentPlugin(['NODE_ENV', 'CORE_HOSTNAME', 'CORE_PORT'])
+        new webpack.EnvironmentPlugin(['NODE_ENV', 'CORE_HOSTNAME', 'CORE_PORT', 'CORE_PROTOCOL'])
     ],
     devtool : 'source-map'
 };
--- webapp/webpack.config.prod.js
+++ webapp/webpack.config.prod.js
@@ -1,29 +1,30 @@
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var webpack = require('webpack');
+var path = require("path");
 
 module.exports = {
-    entry: [
-        './src/index.js',
-    ],
-    output: {
-        path: __dirname + '/dist',
-        filename: "index_bundle.js",
-    },
-    module: {
-        loaders: [
-            { test: /\.html$/, loader: "raw-loader" },
-            { test: /\.js$/, exclude: [/node_modules/], loaders: ['babel-loader']},
-            { test: /\.css$/, loader: "style-loader!css-loader" },
-            { test: /\.png$/, loader: "url-loader?limit=100000" },
-            { test: /\.jpg$/, loader: "file-loader" },
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: __dirname + '/src/index.html',
-            filename: 'index.html',
-            inject: 'body'
-        }),
-        new webpack.EnvironmentPlugin(['NODE_ENV', 'CORE_HOSTNAME', 'CORE_PORT', 'CORE_PROTOCOL'])
+  entry: {
+    app: ['./src/index.js']
+  },
+  output: {
+    path: path.resolve(__dirname, '/dist'),
+    filename: "index.bundle.js",
+  },
+  module: {
+    loaders: [
+      { test: /\.html$/, loader: "raw-loader" },
+      { test: /\.js$/, exclude: [/node_modules/], loaders: ['babel-loader']},
+      { test: /\.css$/, loader: "style-loader!css-loader" },
+      { test: /\.png$/, loader: "url-loader?limit=100000" },
+      { test: /\.jpg$/, loader: "file-loader" }
     ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src/index.html'),
+      filename: 'index.html',
+      inject: 'body'
+    }),
+    new webpack.EnvironmentPlugin(['NODE_ENV', 'CORE_HOSTNAME', 'CORE_PORT', 'CORE_PROTOCOL'])
+  ]
 };
